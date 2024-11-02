55a2ed445d5d2a3a587c6290d7d1edc16022b653
Naresh Karthigeyan <nareshkarthigeyan.2005@gmail.com>
2024-11-02 12:28:27 +0530

    tournament view

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,8 @@
 #include <random>   
 #include <ranges>
 #include <algorithm>
+#include <sstream>
+#include <string>
 
 using namespace std;
 using namespace std::chrono;
@@ -15,20 +17,48 @@ class Algorithm {
         int id;
         int position;
         int tries;
-        double fastestTime;
-        double slowestTime;
-        double averageTime;
-        double currentTime;
+        int points;
+        int matches;
+        int wins;
+        int losses;
+        int ties;
+        long double fastestTime;
+        long double slowestTime;
+        long double totalTime;
+        long double currentTime;
 
         Algorithm(string n, int i){
             name = n;
             id = i;
             position = 0;
             tries = 0;
-            fastestTime = 10000000;
+            fastestTime = 10000000000;
             slowestTime = 0;
             currentTime = 0;
-            averageTime = 0;
+            totalTime = 0;
+            points = 0;
+        }
+
+        string timeAsString(int i){
+            long double time;
+            switch (i)
+            {
+            case 1:
+                time = fastestTime;
+                break;
+            case 2:
+                time = slowestTime;
+                break;
+            case 3:
+                time = totalTime;
+                break;
+            default:
+                time = currentTime;
+                break;
+            }
+            ostringstream output;
+            output << time / 1000000000 << "s";
+            return output.str();
         }
 
         virtual void sort(vector<int> a){};
@@ -101,6 +131,87 @@ class BubbleSort : public Algorithm {
         auto duration = duration_cast<nanoseconds>(stop - start);
         double totalTime = duration.count();
         currentTime = totalTime;
+        if(fastestTime > totalTime || fastestTime == 0) fastestTime = totalTime;
+        if(slowestTime < totalTime || slowestTime == 0) slowestTime = totalTime;
+        tries++;
+    }
+
+};
+class MergeSort : public Algorithm {
+
+    void merge(vector<int>& arr, int left, int mid, int right)
+    {
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+
+        // Create temp vectors
+        vector<int> L(n1), R(n2);
+
+        // Copy data to temp vectors L[] and R[]
+        for (int i = 0; i < n1; i++)
+            L[i] = arr[left + i];
+        for (int j = 0; j < n2; j++)
+            R[j] = arr[mid + 1 + j];
+
+        int i = 0, j = 0;
+        int k = left;
+
+        // Merge the temp vectors back 
+        // into arr[left..right]
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            }
+            else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        // Copy the remaining elements of L[], 
+        // if there are any
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+
+        // Copy the remaining elements of R[], 
+        // if there are any
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+
+    // begin is for left index and end is right index
+    // of the sub-array of arr to be sorted
+    void mergeSort(vector<int>& arr, int left, int right)
+    {
+        if (left >= right)
+            return;
+
+        int mid = left + (right - left) / 2;
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+        merge(arr, left, mid, right);
+    }
+
+    public:
+    MergeSort() : Algorithm("Merge Sort", 3){};
+
+    void sort(vector<int> a){
+        auto start = high_resolution_clock::now();
+
+        mergeSort(a, 0, a.size() - 1);
+
+        auto stop = high_resolution_clock::now();
+        auto duration = duration_cast<nanoseconds>(stop - start);
+        double totalTime = duration.count();
+        currentTime = totalTime;
         if(fastestTime > totalTime) fastestTime = totalTime;
         if(slowestTime < totalTime) slowestTime = totalTime;
         tries++;
@@ -108,10 +219,68 @@ class BubbleSort : public Algorithm {
 
 };
 
+class Tournament {
+    int matchesPlayed = 0;
+    vector<Algorithm*> algos;
 
+    public:
+    Tournament() {
+        algos.push_back(new SelectionSort());
+        algos.push_back(new BubbleSort());
+        algos.push_back(new MergeSort());
+    }
+
+    void playMatch(Algorithm *team1, Algorithm *team2, vector<int> a){
+        team1->sort(a);
+        team2->sort(a);
+
+        team1->matches++;
+        team2->matches++;
+
+        if(team1->currentTime < team2->currentTime){
+            team1->points += 3;
+            team1->wins++;
+            team2->losses++;
+        } else if (team2->currentTime < team1->currentTime){
+            team2->points += 3;
+            team2->wins++;
+            team1->losses++;
+        } else {
+            team1->points++;
+            team2->points++;
+            team1->ties++;
+            team2->ties++;
+        }
+    }
+    void playTournament(vector<int> arr){
+        for (auto a: algos){
+            for(auto b: algos){
+                if(a->id == b->id) continue;
+                playMatch(a, b, arr);
+            }
+        }
+    }
+
+    void pointsTable(){
+        vector<Algorithm*> sortedAlg = algos;
+
+        sort(sortedAlg.begin(), sortedAlg.end(), [](Algorithm* a, Algorithm*  b){
+            if(a->points == b->points)
+                return a->fastestTime < b->fastestTime;
+            return a->points > b->points;
+        });
+
+        cout << "Pos\tAlgorithm\tMatches\tWins\tLosses\tTies\tFastestTime\tSlowestTime\tPoints" << endl;
+        int pos = 1;
+        for(auto a: sortedAlg){
+            a->position = pos;
+            pos++;
+            cout << a->position << "\t" << a->name << "\t" << a->matches << "\t" << a->wins << "\t" << a->losses << "\t" << a->ties << "\t" << a->timeAsString(1) << "\t" << a->timeAsString(2) << "\t" << a->points << endl; 
+        }
+    }
+};
 
 int main(void){
-   cout << "start" << endl;
    random_device rd;
    uniform_int_distribution d(1, 100);
 
@@ -122,13 +291,7 @@ int main(void){
         a.push_back(num);
    }
 
-
-    SelectionSort ss;
-    ss.sort(a);
-    ss.display();
-
-
-    BubbleSort bs;
-    bs.sort(a);
-    bs.display();
+    Tournament tourney;
+    tourney.playTournament(a);
+    tourney.pointsTable();
 }
